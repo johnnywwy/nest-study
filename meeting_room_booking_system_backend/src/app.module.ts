8025e1fbff65875806b7f8debe8d5d3618f210bc
wuwenyu <johnnywwy@gmail.com>
2024-07-25 10:03:28 +0800

    feat: 实现数据库实体

--- meeting_room_booking_system_backend/src/app.module.ts
+++ meeting_room_booking_system_backend/src/app.module.ts
@@ -4,18 +4,23 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 
+// 数据库实体
+import { User } from './user/entities/user.entity';
+import { Role } from './user/entities/role.entity';
+import { Permission } from './user/entities/permission.entity';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: '8.138.58.233',
-      port: 3306,
+      port: 13306,
       username: 'root',
       password: '147258369',
       database: 'meeting_room_booking_system',
       synchronize: true,
       logging: true,
-      entities: [],
+      entities: [User, Role, Permission],
       poolSize: 10,
       connectorPackage: 'mysql2',
       extra: {
