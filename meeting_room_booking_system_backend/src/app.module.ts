721f44440d2e2d24d5ff7fc9a2c878693f068052
wuwenyu <johnnywwy@gmail.com>
2024-07-25 09:16:23 +0800

    feat: 安装数据库

--- meeting_room_booking_system_backend/src/app.module.ts
+++ meeting_room_booking_system_backend/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '8.138.58.233',
+      port: 3306,
+      username: 'root',
+      password: '147258369',
+      database: 'meeting_room_booking_system',
+      synchronize: true,
+      logging: true,
+      entities: [],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
