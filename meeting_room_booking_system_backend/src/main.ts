f24225d35b2774e8606e73b4ac09818e6affff40
johnnywwy <johnnywwy@gmail.com>
2024-07-29 23:27:58 +0800

    feat: 引入全局验证管道 配置swagger

--- meeting_room_booking_system_backend/src/main.ts
+++ meeting_room_booking_system_backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 配置全局验证管道
+  app.useGlobalPipes(new ValidationPipe());
+
+  // 配置swagger
+  const config = new DocumentBuilder()
+    .setTitle('会议室预定系统')
+    .setDescription('一个 React + AntD + Nest + Nginx + Mysql +  Redis 项目')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- meeting_room_booking_system_backend/src/user/user.controller.ts
+++ meeting_room_booking_system_backend/src/user/user.controller.ts
@@ -11,37 +11,47 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('用户管理模块')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  // @ApiQuery({ name: 'username', required: false })
+
+  @ApiOperation({ summary: '用户注册' }) // 这里描述接口的功能
   @Post('register')
   register(@Body() registerUser: RegisterUserDto) {
     console.log(registerUser);
     return 'success';
   }
 
+  @ApiOperation({ summary: '用户登录' })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
+  @ApiOperation({ summary: '获取所有用户' })
   @Get()
   findAll() {
     return this.userService.findAll();
   }
 
+  @ApiOperation({ summary: '获取单个用户' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
 
+  @ApiOperation({ summary: '更新用户' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
+  @ApiOperation({ summary: '删除用户' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
