489a764e3d1675f0bcdb40cb698a6f3a5ad77908
wuwenyu <johnnywwy@gmail.com>
2024-07-29 21:08:29 +0800

    feat: 登录接口和类型

--- meeting_room_booking_system_backend/src/user/user.controller.ts
+++ meeting_room_booking_system_backend/src/user/user.controller.ts
@@ -1,11 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  register(@Body() registerUser: RegisterUserDto) {
+    console.log(registerUser);
+    return 'success';
+  }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
